a838d41c88b7d5814faabe36eba9f7cf251a3fb5
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2023-09-04 17:04:09 +0300

    Add tests

---
+++ packages/dropdown_button2/test/dropdown_button2_test.dart
@@ -0,0 +1,89 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'Button and Menu Focus',
+    () {
+      final List<int> menuItems = List<int>.generate(10, (int index) => index);
+      final value = menuItems.first;
+
+      final dropdownButton = find.byType(DropdownButton2<int>);
+      final dropdownButtonText = find.descendant(of: dropdownButton, matching: find.text('$value'));
+      final dropdownMenu = find.byType(ListView);
+      final dropdownMenuText = find.descendant(of: dropdownMenu, matching: find.text('$value'));
+
+      testWidgets('onTap should request focus for both button and menu',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: DropdownButton2<int>(
+                  value: value,
+                  items: menuItems.map<DropdownItem<int>>((int item) {
+                    return DropdownItem<int>(
+                      value: item,
+                      child: Text(item.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+              ),
+            ),
+          ),
+        );
+
+        final buttonFocusNode = Focus.of(tester.element(dropdownButtonText));
+
+        expect(dropdownMenu, findsNothing);
+        expect(buttonFocusNode.hasFocus, isFalse);
+
+        await tester.tap(dropdownButtonText);
+        await tester.pumpAndSettle();
+
+        expect(dropdownMenu, findsOneWidget);
+        final menuFocusNode = Focus.of(tester.element(dropdownMenuText));
+
+        expect(menuFocusNode.hasPrimaryFocus, isTrue);
+        expect(buttonFocusNode.hasFocus, isTrue);
+      });
+
+      testWidgets('button should stay highlighted when menu closes', (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: DropdownButton2<int>(
+                  value: value,
+                  items: menuItems.map<DropdownItem<int>>((int item) {
+                    return DropdownItem<int>(
+                      value: item,
+                      child: Text(item.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+              ),
+            ),
+          ),
+        );
+
+        final buttonFocusNode = Focus.of(tester.element(dropdownButtonText));
+
+        expect(buttonFocusNode.hasFocus, isFalse);
+
+        await tester.tap(dropdownButtonText);
+        await tester.pumpAndSettle();
+
+        expect(buttonFocusNode.hasFocus, isTrue);
+
+        await tester.tap(dropdownMenuText);
+        await tester.pumpAndSettle();
+
+        expect(buttonFocusNode.hasPrimaryFocus, isTrue);
+      });
+    },
+  );
+}
