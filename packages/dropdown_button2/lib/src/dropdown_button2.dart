f745124a981e48bf84c2ebb4784896113f4469d5
Ahmed Elsayed <ahmedelsaayid@gmail.com>
2026-04-27 01:29:29 +0300

    Document `DropdownButtonFormField2.onChanged` ordering relative to `Form.onChanged`

--- packages/dropdown_button2/lib/src/dropdown_button2.dart
+++ packages/dropdown_button2/lib/src/dropdown_button2.dart
@@ -1236,6 +1236,10 @@ class DropdownButtonFormField2<T> extends FormField<T> {
        );
 
   /// {@macro flutter.material.dropdownButton.onChanged}
+  ///
+  /// This callback is invoked after the parent [Form]'s [Form.onChanged] callback.
+  /// The field's updated value is available in the [Form.onChanged] callback
+  /// via [FormFieldState.value].
   final ValueChanged<T?>? onChanged;
 
   /// The decoration to show around the dropdown button form field.
